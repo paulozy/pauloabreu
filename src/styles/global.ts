f99462bffd5fe1f7d362b8974e2ac936ecce263a
Paulo Abreu <paulo.santana@luby.software>
2023-07-07 10:35:03 -0300

    chore: config default theme and global style

---
+++ src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding:0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${(props) => props.theme['blue-500']};
+  }
+
+  body {
+    background: ${(props) => props.theme['blue-800']};
+    color: ${(props) => props.theme['white']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font: 400 1rem Roboto, sans-serif;
+  }
+`
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,9 @@
+export const defaultTheme = {
+  white: '#fff',
+
+  'blue-400': '#096cac',
+  'blue-500': '#075d9a',
+  'blue-600': '#054d89',
+  'blue-700': '#023e77',
+  'blue-800': '#002e65',
+} as const
